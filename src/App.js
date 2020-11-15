e33d238a59f8b503015d6381af8182d779c8928d
taofeng <tfeng@launchfire.com>
2020-11-14 19:08:47 -0500

    gallery set up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,124 @@
 import Unsplash from 'unsplash-js';
+import React from 'react';
+import _ from 'lodash';
 
-import logo from './logo.svg';
+// import ReactDOM from 'react-dom';
+//
+// import logo from './logo.svg';
 import './App.css';
+import './gallery.css';
 
 // TODO: Replace "APP_ACCESS_KEY" with your own key, which
 // can be generated here: https://unsplash.com/developers
-const unsplash = new Unsplash({ accessKey: 'APP_ACCESS_KEY' });
+const unsplash = new Unsplash({accessKey: '08LdVyqWqRjW9u94CuN2T9yaUx-tVA-X4h4IuQGw2eI'});
+const toJson = require('unsplash-js').toJson;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//the number of loading images for initial loading and each scroll
+const loadingImageNum = 10;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: true,
+            images: {},
+        };
+        this.prev = window.scrollY;
+
+
+        this.throttleHandleChange = _.debounce(this.throttleHandleChange, 500);
+    }
+
+    componentDidMount() {
+        this.loadImages();
+        window.addEventListener('scroll', e => this.handleScroll(e));
+    }
+
+    //scroll down to load more images
+    handleScroll = (e) => {
+        const window = e.currentTarget;
+        if (this.prev < window.scrollY) {
+            this.throttleHandleChange();
+        }
+        this.prev = window.scrollY;
+    };
+
+    throttleHandleChange = () => this.loadImages();
+
+    //init the gallery, include fetching Random Photos
+    loadImages() {
+        unsplash.photos.getRandomPhoto({count: loadingImageNum})
+            .then(toJson)
+            .then(json => {
+                this.setState({images: json, isLoading: false});
+            });
+    }
+
+    render() {
+        const isLoading = this.state.isLoading;
+        return (
+            <div>
+                <Gallery images={this.state.images}/>
+                {isLoading && <LoadingScreen />}
+            </div>
+        );
+    }
+}
+
+function LoadingScreen() {
+    return (
+        <div className="loading ">
+            <div className="loading-icon"> </div>
+            <p>Loading...</p>
+        </div>
+    );
+}
+
+class Gallery extends React.Component {
+    state = {
+        thumbs: [],
+    };
+
+    handleClick(id) {
+        console.log(id);
+    }
+
+    renderThumb(images) {
+        console.log(images);
+        const thumbs = this.state.thumbs;
+
+        if(images.length > 0) {
+            thumbs.push(images.map((data) => {
+                return (<Thumb url={data.urls && data.urls.thumb} key={data.id} desc={data.alt_description} onClick={() => this.handleClick(data.id)}/>);
+            }));
+        }
+
+        if (this.state.thumbs.length < thumbs.length) {
+            this.setState({thumbs:thumbs});
+        }
+
+        return thumbs;
+    }
+
+    render() {
+        return (
+            <div className="gallery">
+                <h2>Gallery wall</h2>
+                <div className="waterfall">{this.renderThumb(this.props.images)}</div>
+            </div>
+        );
+    }
+}
+
+function Thumb (props) {
+    return (
+        <div className="item inbound" onClick={props.onClick}>
+            <img
+                src={props.url}
+                alt={props.desc}
+            />
+        </div>
+    );
 }
 
 export default App;
